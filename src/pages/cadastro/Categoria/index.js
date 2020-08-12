f7fc7cd3a6f724171b176cc235f881a44a8e19eb
Virgínia <gcv.virginia@gmail.com>
2020-08-12 07:47:10 -0300

    Modificações da quarta aula

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,7 +1,8 @@
-import React, {useState} from 'react';
-import PageDefault from '../../../components/PageDefault';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import PageDefault from '../../../components/PageDefault';
 import FormField from '../../../components/FormField';
+import Button from '../../../components/Button'
 
 function CadastroCategoria() {
   const valoresIniciais = {
@@ -22,18 +23,48 @@ function CadastroCategoria() {
 
   function handleChange(infosDoEvento) {
     setValue(
-      infosDoEvento.target.getAttribute('name'), 
+      infosDoEvento.target.getAttribute('name'),
       infosDoEvento.target.value
     );
   }
+
+  useEffect(() => {
+    console.log('alo brasil');
+    const URL_TOP = 'http://localhost:8080/categorias';
+    fetch(URL_TOP)
+      .then(async (respostaDoServidor) => {
+      const resposta = await respostaDoServidor.json();
+      setCategorias([
+        ...resposta
+      ]);
+    });
+    /*setTimeout(() => {
+      setCategorias([
+        ...categorias,
+          {
+            "id": 1,
+            "nome": "Front End",
+            "descricao": "Uma categoria bacanuda",
+            "cor": "#cbd1ff"
+          },
+          {
+            "id": 1,
+            "nome": "Back end",
+            "descricao": "Outra categoria bacanuda",
+            "cor": "#cbd1ff"
+          }
+      ])
+    }, 4 * 1000);*/
+  }, [])
+
 
   return (
     <PageDefault>
       <h1>Cadastro de Categoria: {values.nome}</h1>
       <form onSubmit={function handleSubimit(event) {
         event.preventDefault();
-        //...categorias é como se tivesse falando pega todas as categorias sem jogar fora e 
-        //adiciona essa nova categoria.
+        /* ...categorias é como se tivesse falando pega todas as categorias sem jogar fora e
+        adiciona essa nova categoria. */
         setCategorias([...categorias, values]);
         setValues(valoresIniciais)
       }}>
@@ -44,17 +75,13 @@ function CadastroCategoria() {
           value={values.nome}
           onChange={handleChange}
         />
-        <div>
-          <label>
-            Descrição:
-            <textarea
-              type="text"
-              value={values.descricao}
-              name="descricao"
-              onChange={handleChange}
-            />
-          </label>
-        </div>
+        <FormField
+          type="textarea"
+          name="descricao"
+          label="Descrição:"
+          value={values.descricao}
+          onChange={handleChange}
+        />
         <FormField
           type="color"
           name="cor"
@@ -62,23 +89,16 @@ function CadastroCategoria() {
           value={values.cor}
           onChange={handleChange}
         />
-        {/*<div>
-          <label>
-            Cor:
-            <input
-              type="color"
-              value={values.cor}
-              name="cor"
-              onChange={handleChange}
-            />
-          </label>
-        </div>*/}
-        
-
-        <button>
+        <Button>
           Cadastrar
-        </button>
+        </Button>
       </form>
+
+      {categorias.length === 0 && (
+        <div>
+          Loading...
+        </div>
+      )}
 
       <ul>
         {categorias.map((categoria, indice) => {
@@ -89,7 +109,7 @@ function CadastroCategoria() {
           )
         })}
       </ul>
-      
+
       <Link to="/">
         Ir para home
       </Link>
